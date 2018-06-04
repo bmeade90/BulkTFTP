35d39bf437f348ef573f5471bb3f8ba7bbc0c89d
Brian Meade <bmeade90@vt.edu>
2018-06-04 14:46:23 -0400

    Updating comments

--- CUCM-Bulk-TFTP-Upload.py
+++ CUCM-Bulk-TFTP-Upload.py
@@ -1,13 +1,18 @@
+# # This program will traverse a local directory recursively and upload 
+# all files while maintaining the directory structure.
+#
 # Install Python 2.7 and choose the option to add to path (off by default)
 # Then install two modules
 #  C:\>pip install requests
 #  C:\>pip install BeautifulSoup4
+#
 # May need to use "python -m pip install requests"
+#
 # Then run the program CUCM-Bulk-TFTP-Upload.py
 #  C:\>python tftp.py hostname/IP username password localdirectory
+#
 # Windows formatting for local directory- C:\\TFTP-Root\\tftpfiles
-# This program will traverse a local directory recursively and upload 
-# all files while maintaining the directory structure.
+#
 # You can pass in the parameters on the command line or edit the
 # sys.argv line at the bottom of the script.
 
